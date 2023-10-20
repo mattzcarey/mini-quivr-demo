c58cab1a013785baf44e9e52569b550884ecc2cb
Matt <77928207+mattzcarey@users.noreply.github.com>
2023-10-20 11:00:44 +0200

    fix: have a chatbot

--- demo/main.py
+++ demo/main.py
@@ -15,7 +15,7 @@ from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
 
 from demo.constants.paths import ROOT_FOLDER
 from demo.llm.handlers import PrintRetrievalHandler, StreamHandler
-from demo.llm.llm import get_qa_chain
+from demo.llm.llm import get_llm_chain, get_qa_chain
 from demo.prompts.welcome_message import build_welcome_message
 
 st.set_page_config(
@@ -26,19 +26,20 @@ st.set_page_config(
 
 st.title("Mini Quivr 🤖")
 
-uploaded_files = st.sidebar.file_uploader(
-    label="Upload PDF files", type=["pdf"], accept_multiple_files=True
-)
-if not uploaded_files:
-    st.info("Please upload PDF documents to continue.")
-    st.stop()
+# uploaded_files = st.sidebar.file_uploader(
+#     label="Upload PDF files", type=["pdf"], accept_multiple_files=True
+# )
+# if not uploaded_files:
+#     st.info("Please upload PDF documents to continue.")
+#     st.stop()
 
 # Setup memory for contextual conversation
 msgs = StreamlitChatMessageHistory()
 memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=msgs, return_messages=True)
 
 # Get LLM chain
-chain = get_qa_chain(uploaded_files, memory)
+chain = get_llm_chain()
+# chain = get_qa_chain(uploaded_files, memory)
 
 # Clear History
 if len(msgs.messages) == 0 or st.sidebar.button("Clear message history"):
@@ -53,12 +54,17 @@ for msg in msgs.messages:
 
 # Query LLM
 if user_query := st.chat_input(placeholder="Ask me anything!"):
+    #comment this for the weird memory thing
     msgs.add_user_message(user_query)
     st.chat_message("user").write(user_query)
 
     with st.chat_message("assistant"):
         # Define callback handlers
-        retrieval_handler = PrintRetrievalHandler(st.container())
+        # retrieval_handler = PrintRetrievalHandler(st.container())
         stream_handler = StreamHandler(st.empty())
 
-        response = chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
+        response = chain.run(question=user_query)
+        msgs.add_ai_message(response)
+        st.write(response)
+
+        # response = chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
